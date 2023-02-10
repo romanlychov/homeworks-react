d0bc0fa9bd3f7bf649774aa13587c627f6dd5c13
RomanLychov <romanlychov@gmail.com>
2023-02-10 21:37:48 +0200

    done header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import './App.css';
-import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Sidebar from './Components/Sidebar/Sidebar';
-import Main from './Components/Main/Main';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Header />
         <Sidebar />
-        <Main />
-        <Footer />
       </div>
     );
   }
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import './Sidebar.module.css';
+import './Sidebar.css';
 
 class Sidebar extends React.Component {
-
+    render() {
+        return (
+            <div className='sidebar'>
+                <article className='sidebar__'></article>
+                <article className='sidebar__'></article>
+                <article className='sidebar__'></article>
+            </div>
+        );
+    }
 }
 
 export default Sidebar;
