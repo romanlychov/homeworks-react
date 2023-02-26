be3f7185ecc03ac6fc25a597d963b58ceb1cf28b
RomanLychov <romanlychov@gmail.com>
2023-02-26 16:35:56 +0200

    minor fixes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,5 @@ import './index.module.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
