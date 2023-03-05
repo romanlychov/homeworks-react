64f46d59e7070ccad4d798ca7cb2c2e0b785b3ee
RomanLychov <romanlychov@gmail.com>
2023-03-05 20:59:08 +0200

    Rewrote Header and MainPeople components on hooks

--- src/App.js
+++ src/App.js
@@ -1,232 +1,49 @@
-import React from 'react';
 import style from './App.module.css';
-// import Footer from './Components/Footer/Footer';
-// import Header from './Components/Header/Header';
-// import Main from './Components/Main/Main';
-// import Sidebar from './Components/Sidebar/Sidebar';
-// import { v4 as idGenerator } from 'uuid';
 import MainPeople from './Components/SWAPI/Main/MainPeople';
 import BeatLoader from 'react-spinners/BeatLoader';
 import Header from './Components/SWAPI/Header/Header';
 import MainPlanets from './Components/SWAPI/Main/MainPlanets';
 import MainStarships from './Components/SWAPI/Main/MainStarships';
-// import PhoneBookForm from './Components/Main/PhoneBook/PhoneBookForm/PhoneBookForm';
+import { useState } from 'react';
 
-// TODO List
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
+const App = () => {
+  const [request, setRequest] = useState([]);
+  const [wasClicked, setWasClicked] = useState('');
+  const [loading, setLoading] = useState(false);
 
-//     this.state = {
-//       todoList: [],
-//       inputValue: '',
-//     };
-
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//     this.handleDone = this.handleDone.bind(this);
-//   }
-
-//   handleChange(e) {
-//     this.setState({ inputValue: e.target.value });
-//   }
-
-//   handleSubmit(e) {
-//     e.preventDefault();
-//     if (this.state.inputValue) {
-//       const newItem = {
-//         text: this.state.inputValue,
-//         id: idGenerator(),
-//         done: false,
-//       };
-//       this.setState({
-//         todoList: [newItem, ...this.state.todoList],
-//         inputValue: '',
-//       });
-//     }
-//   }
-
-//   handleDone(e) {
-//     const todoList = [...this.state.todoList];
-//     todoList.map(element => {
-//       if (e.id === element.id) {
-//         element.done = !element.done;
-//       }
-//       return element;
-//     });
-//     this.setState({ todoList });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <Header />
-//         <div className={style.wrapper}>
-//           <Sidebar />
-//           <Main state={this.state}
-//             handleChange={this.handleChange}
-//             handleSubmit={this.handleSubmit}
-//             handleDone={this.handleDone}
-//           />
-//         </div>
-//         <Footer />
-//       </div>
-//     );
-//   }
-
-// }
-
-
-// Phone Book
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       phoneBookForm: false,
-//       phoneBook: [],
-//       firstNameValue: '',
-//       lastNameValue: '',
-//       phoneNumberValue: '',
-//     };
-
-//     this.handleChange = this.handleChange.bind(this);
-//     this.onClickAddNewContactBtn = this.onClickAddNewContactBtn.bind(this);
-//     this.addContactButton = this.addContactButton.bind(this);
-//     this.cancelButton = this.cancelButton.bind(this);
-//     this.deleteContactButton = this.deleteContactButton.bind(this);
-//   }
-
-//   handleChange = (e) => {
-//     this.setState({ [e.target.name]: e.target.value });
-//   }
-
-//   onClickAddNewContactBtn = () => {
-//     this.setState({
-//       phoneBookForm: !this.state.phoneBookForm,
-//       firstNameValue: '',
-//       lastNameValue: '',
-//       phoneNumberValue: '',
-//     });
-//   }
-
-//   addContactButton = (e) => {
-//     e.preventDefault();
-//     let areAllFieldsFilled = Boolean(this.state.firstNameValue && this.state.lastNameValue && this.state.phoneNumberValue);
-//     if (areAllFieldsFilled) {
-//       const newContact = {
-//         name: this.state.firstNameValue,
-//         lastName: this.state.lastNameValue,
-//         phoneNumber: this.state.phoneNumberValue,
-//         id: idGenerator(),
-//       };
-//       this.setState({
-//         phoneBookForm: false,
-//         phoneBook: [...this.state.phoneBook, newContact],
-//         firstNameValue: '',
-//         lastNameValue: '',
-//         phoneNumberValue: '',
-//       });
-//     } else {
-//       alert('All fields must be filled');
-//     }
-//   }
-
-//   cancelButton = (e) => {
-//     e.preventDefault();
-//     this.setState({
-//       phoneBookForm: false,
-//       firstNameValue: '',
-//       lastNameValue: '',
-//       phoneNumberValue: '',
-//     });
-//   }
-
-//   deleteContactButton = (e) => {
-//     let phoneBook = [...this.state.phoneBook];
-//     this.setState({ phoneBook: phoneBook.filter(item => item.id !== e) });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <Header />
-//         <div className={style.wrapper}>
-//           <Sidebar />
-//           <Main
-//             addNewContact={this.onClickAddNewContactBtn}
-//             state={this.state}
-//             handleChange={this.handleChange}
-//             addContactButton={this.addContactButton}
-//             cancelButton={this.cancelButton}
-//             deleteContactButton={this.deleteContactButton}
-//           />
-//         </div>
-//         <PhoneBookForm
-//           state={this.state}
-//           handleChange={this.handleChange}
-//           addContactButton={this.addContactButton}
-//           cancelButton={this.cancelButton}
-//         />
-//         <Footer />
-//       </div>
-//     );
-//   }
-
-// }
-
-// The Star Wars API
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      queryResult: [],
-      loading: false,
-      wasClicked: '',
-    }
+  const fetchData = async (request) => {
+    setRequest([]);
+    setLoading(true);
+    const response = await fetch(`https://swapi.dev/api/${request}`)
+    const jsonData = await response.json();
+    setRequest(jsonData.results);
+    setWasClicked(request);
+    setLoading(false)
   }
 
-  getRequest = async (req) => {
-    this.setState({
-      queryResult: [],
-      loading: !this.state.loading
-    })
-    await fetch(`https://swapi.dev/api/${req}`)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          queryResult: data.results,
-          loading: !this.state.loading,
-          wasClicked: req,
-        })
-      });
-  }
-
-  showList = (wasClicked) => {
+  const showList = (wasClicked) => {
     switch (wasClicked) {
       case 'people':
-        return <MainPeople state={this.state} />;
+        return <MainPeople request={request} />;
       case 'planets':
-        return <MainPlanets state={this.state} />;
+        return <MainPlanets request={request} />;
       case 'starships':
-        return <MainStarships state={this.state} />;
+        return <MainStarships request={request} />;
       default:
         return null;
     }
   }
 
-  render() {
-    return (
-      <div>
-        <Header getRequest={this.getRequest} />
-        {this.showList(this.state.wasClicked)}
-        <div className={style.center}>
-          <BeatLoader color="#ff6" loading={this.state.loading} size={75} />
-        </div>
+  return (
+    <div>
+      <Header fetchData={fetchData} />
+      {showList(wasClicked)}
+      <div className={style.center}>
+        <BeatLoader color="#ff6" loading={loading} size={75} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
+
--- src/Components/SWAPI/Main/MainPeople.jsx
+++ src/Components/SWAPI/Main/MainPeople.jsx
@@ -1,55 +1,37 @@
-import React from "react";
+import { useState } from 'react';
 import style from './Main.module.css';
 
-class MainPeople extends React.Component {
-    state = {
-        person: {},
-        visible: false,
-    }
+const MainPeople = (props) => {
+    const [person, setPerson] = useState({});
+    const [visible, setVisible] = useState(false)
 
-    setPerson = (name, birth_year, gender, hair_color) => {
-        this.setState({
-            person: {
-                name: name,
-                birth_year: birth_year,
-                gender: gender,
-                hair_color: hair_color,
-            },
-            visible: true
-        })
-    }
 
-    onClickVis = () => {
-        this.setState({ visible: false })
-    }
+    return (
+        <main>
+            <ul className={style.list}>
+                {props.request.map(item => (
+                    <li key={item.name}
+                        onClick={() => {
+                            setPerson({ name: item.name, birth_year: item.birth_year, gender: item.gender, hair_color: item.hair_color })
+                            setVisible(true);
+                        }}
+                        className={style.listItem}
+                    >{item.name}</li>
+                ))}
+            </ul>
 
-    render() {
-        return (
-            <main>
-                <ul className={style.list}>
-                    {this.props.state.queryResult.map(item => (
-                        <li key={item.name}
-                            onClick={() => {
-                                this.setPerson(item.name, item.birth_year, item.gender, item.hair_color)
-                            }}
-                            className={style.listItem}
-                        >{item.name}</li>
-                    ))}
-                </ul>
+            <div className={visible ? style.visible : style.unvisible}>
+                <article className={style.card}>
+                    <h3 className={style.cardTitle}>{person.name}</h3>
+                    <p>Birth year: {person.birth_year}</p>
+                    <p>Gender: {person.gender}</p>
+                    <p>Hair color: {person.hair_color}</p>
+                    <button className={style.cardBTN} onClick={() => setVisible(false)}>Close</button>
+                </article>
+            </div>
 
-                <div className={this.state.visible ? style.visible : style.unvisible}>
-                    <article className={style.card}>
-                        <h3 className={style.cardTitle}>{this.state.person.name}</h3>
-                        <p>Birth year: {this.state.person.birth_year}</p>
-                        <p>Gender: {this.state.person.gender}</p>
-                        <p>Hair color: {this.state.person.hair_color}</p>
-                        <button className={style.cardBTN} onClick={this.onClickVis}>Close</button>
-                    </article>
-                </div>
-
-            </main>
-        );
-    }
+        </main>
+    );
 }
 
 export default MainPeople;
